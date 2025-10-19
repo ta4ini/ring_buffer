8a70f5becdedec05f9b501b59df9781ff61f667a
ta4ini <wferrit@gmail.com>
2025-10-19 15:21:23 +0300

    Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
-use std::fmt;
+use std::{
+    fmt,
+    sync::{
+        Mutex,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
 
 fn main() -> Result<(), NoSpaceLeft> {
-    let mut rb = create(3);
+    let rb = RingBuffer::create(3);
 
-    write(&mut rb, "ab")?;
+    rb.write("ab")?;
 
-    write(&mut rb, "c")?;
+    rb.write("c")?;
 
     println!("{:?}", rb);
-    println!("Read: {} , Buffer: {:?}", read(&mut rb, 1).unwrap(), rb);
+    println!(
+        "Read: {} , Len: {}, Buffer: {:?}",
+        rb.read(1).unwrap(),
+        rb.len(),
+        rb
+    );
 
-    write(&mut rb, "e")?;
+    rb.write("e")?;
     println!("{:?}", rb);
-    println!("Read: {} , Buffer: {:?}", read(&mut rb, 2).unwrap(), rb);
+    println!(
+        "Read: {} , Len: {}, Buffer: {:?}",
+        rb.read(2).unwrap(),
+        rb.len(),
+        rb
+    );
 
-    match write(&mut rb, "fgz") {
+    match rb.write("fgz") {
         Ok(value) => println!("Success: {}", value),
         Err(e) => println!("Error: {}", e),
     }
@@ -35,9 +51,9 @@ fn main() -> Result<(), NoSpaceLeft> {
 
 #[derive(Debug)]
 struct RingBuffer {
-    read_idx: usize,
-    write_idx: usize,
-    data: Vec<u8>,
+    read_idx: AtomicUsize,
+    write_idx: AtomicUsize,
+    data: Mutex<Vec<u8>>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -49,66 +65,109 @@ impl fmt::Display for NoSpaceLeft {
     }
 }
 
-fn create(size: usize) -> RingBuffer {
-    let data = vec![0; size];
-
-    RingBuffer {
-        read_idx: 0,
-        write_idx: 0,
-        data,
+impl RingBuffer {
+    fn create(size: usize) -> Self {
+        Self {
+            read_idx: 0.into(),
+            write_idx: 0.into(),
+            data: Mutex::new(vec![0; size]),
+        }
     }
-}
 
-fn write(rb: &mut RingBuffer, string: &str) -> Result<usize, NoSpaceLeft> {
-    for ch in string.bytes() {
-        if rb.data[rb.write_idx] == 0 {
-            rb.data[rb.write_idx] = ch;
-        } else {
-            return Err(NoSpaceLeft);
+    fn write(&self, string: &str) -> Result<usize, NoSpaceLeft> {
+        for ch in string.bytes() {
+            let mut data = self.data.lock().unwrap();
+            let write_idx = self.write_idx.load(Ordering::Acquire);
+            if data[write_idx] == 0 {
+                data[write_idx] = ch;
+            } else {
+                return Err(NoSpaceLeft);
+            }
+
+            self.write_idx
+                .store((write_idx + 1) % data.len(), Ordering::Relaxed);
         }
 
-        rb.write_idx = (rb.write_idx + 1) % rb.data.len();
+        Ok(self.write_idx.load(Ordering::Relaxed))
     }
 
-    Ok(rb.write_idx)
-}
+    fn read(&self, read_count: usize) -> Option<String> {
+        let mut vec = Vec::new();
 
-fn read(rb: &mut RingBuffer, read_count: usize) -> Option<String> {
-    let mut vec = Vec::new();
+        let read_idx = self.read_idx.load(Ordering::Acquire);
+        self.write_idx.store(read_idx, Ordering::Release);
 
-    rb.write_idx = rb.read_idx;
+        for _ in 0..read_count {
+            let mut data = self.data.lock().unwrap();
+            let ch = data[read_idx];
+            vec.push(ch);
+            data[read_idx] = 0;
+            self.read_idx
+                .store((read_idx + 1) % data.len(), Ordering::Release);
+        }
 
-    for _ in 0..read_count {
-        let ch = rb.data[rb.read_idx];
-        vec.push(ch);
-        rb.data[rb.read_idx] = 0;
-        rb.read_idx = (rb.read_idx + 1) % rb.data.len();
+        String::from_utf8(vec).ok()
     }
 
-    String::from_utf8(vec).ok()
+    fn len(&self) -> usize {
+        self.data
+            .lock()
+            .unwrap()
+            .iter()
+            .filter(|p| **p != 0)
+            .count()
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{sync::Arc, thread};
+    use rand::Rng;
     use super::*;
 
     #[test]
     fn test_1() {
-        let mut rb = create(3);
-        assert_eq!(rb.data.len(), 3);
+        let rb = RingBuffer::create(3);
+        assert_eq!(rb.data.lock().unwrap().len(), 3);
 
-        assert_eq!(write(&mut rb, "ab").unwrap(), 2);
+        assert_eq!(rb.write("ab").unwrap(), 2);
 
-        assert_eq!(write(&mut rb, "c").unwrap(), 0);
+        assert_eq!(rb.write("c").unwrap(), 0);
 
-        assert_eq!(read(&mut rb, 1).unwrap(), "a");
+        assert_eq!(rb.read(1).unwrap(), "a");
 
-        assert_eq!(write(&mut rb, "ec").unwrap_err(), NoSpaceLeft);
+        assert_eq!(rb.write("ec").unwrap_err(), NoSpaceLeft);
+    }
 
-        // assert_eq!(read(&mut rb, 2), "bc");
+    #[test]
+    fn test_2() {
+        let buf_size = 10;
+        let buffer = Arc::new(RingBuffer::create(buf_size));
 
-        // assert_eq!(write(&mut rb, "fghjy"), 0);
+        let mut handles = vec![];
+        for _ in 1..100 {
+            let buffer_ref = Arc::clone(&buffer);
 
-        // assert_eq!(read(&mut rb, 3), "efg");
+            let handle = thread::spawn(move || {
+                for _ in 0..1000 {
+                    if buf_size == buffer_ref.len() {
+                        buffer_ref.write("a").unwrap();
+                    } else {
+                        let mut rng = rand::rng();
+                        buffer_ref.read(rng.random_range(0..=buf_size)).unwrap();
+                    }
+                }
+            });
+
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        let len = buffer.len();
+
+        assert!(len <= 10);
     }
 }
